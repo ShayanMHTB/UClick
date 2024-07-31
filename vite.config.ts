9c95c2040baadf00beb8db90007d2995f073c2a7
Shayan Mahtabi <shayan.mhtb@gmail.com>
2024-07-31 13:00:12 +0200

    configuration files updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@/components': resolve(__dirname, 'src/components'),
+      '@/pages': resolve(__dirname, 'src/pages'),
+      '@/services': resolve(__dirname, 'src/services'),
+      '@/hooks': resolve(__dirname, 'src/hooks'),
+      '@/contexts': resolve(__dirname, 'src/contexts'),
+      '@/types': resolve(__dirname, 'src/types'),
+      '@/utils': resolve(__dirname, 'src/utils'),
+      '@/constants': resolve(__dirname, 'src/constants'),
+      '@/theme': resolve(__dirname, 'src/theme'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
-})
+  },
+});
